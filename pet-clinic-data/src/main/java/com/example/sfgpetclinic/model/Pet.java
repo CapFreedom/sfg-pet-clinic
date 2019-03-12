93e1df38fbab41a9ea4e98d75d4b7e55da3c5660
CapFreedom <a9122297039@ya.ru>
2019-03-12 12:09:19 +0500

    Add base entity with long id. Closes #10.

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.example.sfgpetclinic.model;
 
 import java.time.LocalTime;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private LocalTime birthDate;
     private PetType petType;
     private Owner owner;
