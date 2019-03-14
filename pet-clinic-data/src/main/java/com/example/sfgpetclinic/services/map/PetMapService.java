e57b9277a784493ae15e018625dbc98e0bbcf4d2
capfreedom <a9122297039@ya.ru>
2019-03-14 18:31:43 +0500

    fixed services

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/PetMapService.java
@@ -1,6 +1,7 @@
 package com.example.sfgpetclinic.services.map;
 
 import com.example.sfgpetclinic.model.Pet;
+import com.example.sfgpetclinic.services.PetService;
 
-public class PetMapService extends AbstractMapService<Pet, Long> {
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
 }
--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/VetMapService.java
@@ -1,6 +1,7 @@
 package com.example.sfgpetclinic.services.map;
 
 import com.example.sfgpetclinic.model.Vet;
+import com.example.sfgpetclinic.services.VetService;
 
-public class VetMapService extends AbstractMapService<Vet, Long> {
+public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 }
